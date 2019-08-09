fb185eb012ede5a5ebb4fa5f7a885a8ce1c9287f
shirishma <51737901+shirishma@users.noreply.github.com>
2019-08-09 22:37:49 +0530

    Create subofhr&min.py

---
+++ subofhr&min.py
@@ -0,0 +1,14 @@
+h1=int(input())
+m1=int(input())
+h2=int(input())
+m2=int(input())
+if(h1>h2):
+    r1=h1-h2
+else:
+    r1=h2-h1   
+if(m1>m2):
+    r2=m1-m2
+else:
+    r2=m2-m1
+print(r1,r2)    
+
